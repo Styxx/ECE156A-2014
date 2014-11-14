40acdb0c64c37a32ca03dad1d4bfef8cf666cd71
Styxx <Styxx_N_Stones@live.com>
2014-11-13 18:39:34 -0800

    In my head, "shift" is spelled "state."

--- machine_b.v
+++ machine_b.v
@@ -1,5 +1,5 @@
 /*
-*  Pattern Detecting Machine - Register Level
+*  Pattern Detecting Machine - Shift Register
 *  Checks for the patterns 0100 or 00010
 *  Rightmost bit arrives first
 */
@@ -10,25 +10,25 @@ module machine_b(z, clk, in, reset);
 	input clk, in, reset;
 	output z;
 
-	reg [4:0] state_reg;
+	reg [4:0] shift_reg;
 	reg z;
 
 	always @ (negedge clk)
 		if (!reset) begin
-			state_reg[4:0] = 5'b00000;
+			shift_reg[4:0] = 5'b00000;
 		end
 		else begin
-			state_reg[4] = state_reg[3];
-			state_reg[3] = state_reg[2];
-			state_reg[2] = state_reg[1];
-			state_reg[1] = state_reg[0];
-			state_reg[0] = in;
+			shift_reg[4] = shift_reg[3];
+			shift_reg[3] = shift_reg[2];
+			shift_reg[2] = shift_reg[1];
+			shift_reg[1] = shift_reg[0];
+			shift_reg[0] = in;
 
-			if ((state_reg[4:0] == 5'b01000) || 
-			   (state_reg[4:0] == 5'b00010) ||
-			   (state_reg[4:0] == 5'b10010) ||
-			   (state_reg[4:0] == 5'b00101) ||
-			   (state_reg[4:0] == 5'b00100))
+			if ((shift_reg[4:0] == 5'b01000) || 
+			   (shift_reg[4:0] == 5'b00010) ||
+			   (shift_reg[4:0] == 5'b10010) ||
+			   (shift_reg[4:0] == 5'b00101) ||
+			   (shift_reg[4:0] == 5'b00100))
 				z = 1;
 			else
 				z = 0;
