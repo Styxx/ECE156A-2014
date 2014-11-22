117b21c87fc7134015163338db4017906f4e4cf1
Styxx <Styxx_N_Stones@live.com>
2014-11-22 01:14:39 -0800

    Established adds and subs

--- Homework3/piggyBank.v
+++ Homework3/piggyBank.v
@@ -3,7 +3,7 @@
 * Adds credit for penny(1), nickel(5), dime(10), and quarter(25)
 * Subtracts credit for apple(75), banana(20), carrot(30), and date(40)
 * Max credit is $2.55, displayed in hex
-* Resets asynchronously
+* Active low, asynchronous reset
 */
 
 module piggyBank (input clk, reset, penny, nickel, dime, quarter, apple, banana, carrot, date, output [7:0] credit);
@@ -11,13 +11,26 @@ module piggyBank (input clk, reset, penny, nickel, dime, quarter, apple, banana,
   output [7:0] credit;
   
   always @ (posedge clk or reset)
-  
-  begin
-  
-  
-  
-  
-  
-  
-  end
+    begin
+      if (!reset)
+        credit = 7'b0000000;
+      else begin
+        if (penny == 1)
+          credit = credit + 1;
+        else if (nickel == 1)
+          credit = credit + 5;
+        else if (dime == 1)
+          credit = credit + 10;
+        else if (quarter == 1)
+          credit = credit + 25;
+        if (apple == 1)
+          credit = credit - 75;
+        else if (banana == 1)
+          credit = credit - 20;
+        else if (carrot == 1)
+          credit = credit - 30;
+        else if (date == 1)
+          credit = credit - 40;
+      end
+    end
 endmodule
