902bc4b32eed27b43f674ecacd6d6c0492034e44
Styxx <Styxx_N_Stones@live.com>
2014-12-03 21:02:18 -0800

    Commented

--- Homework3/vm_tb.sv
+++ Homework3/vm_tb.sv
@@ -7,23 +7,25 @@ class random_buy;
   rand int buyC;                                          //buyChance - chance to buy a product
   rand int prodSel;                                       //productSelection - which product to be bought.
   constraint c { index >= 0; index <=4;                   //0-4 for 4 coins and a no coin signal
-                 buyC >= 0; buyC <=9;                     // 90% no buy, 10% buy
+                 buyC >= 0; buyC <=9;                     //90% no buy, 10% buy
                  prodSel >= 0; prodSel < 4;}              //[00, 01, 10, 11]
 endclass
 
 module vmTestBench();
+/* VENDING MACHINE INSTANTIATION */
   reg clk, reset, serialIn, buy;
   reg [1:0] product;
   wire [6:0] digit0, digit1;
-  VendingMachine VM(clk,reset,serialIn,product,buy, digit0,digit1);
+  VendingMachine VM(clk,reset,serialIn,product,buy, digit0,digit1); 
+/* ~~~~~ */
   
-//Covergroups
+/* COVERGROUPS */
   covergroup Trans_Cov @ (clk);
     PENNY:    coverpoint VM.penny {bins pennyDetected = {1};}    // If the penny signal = 1, then bin is covered
     NICKEL:   coverpoint VM.nickel {bins nickelDetected = {1};}
     DIME:     coverpoint VM.dime {bins dimeDetected = {1};}
     QUARTER:  coverpoint VM.quarter {bins quarterDetected = {1};}
-    // Does not ask to cover a no-coin sequence
+    // Does not ask to cover a no-coin sequence, so no no-coin coverpoint
     
     CREDITS: coverpoint VM.credit {    
       bins a1to4 = { [1:4] };                                  // If credits reach value from 1 to 4, bin is covered
@@ -37,6 +39,7 @@ module vmTestBench();
     }
     
     // Credit coverpoint expanded (for testing / debugging)
+    // I used this section to expand out the credit coverpoint to view which bins were hit
     /*
     A1TO4: coverpoint VM.credit {bins a1to4 = {[1:4]};}
     A5TO9: coverpoint VM.credit {bins a5to9 = {[5:9]};}
@@ -48,8 +51,7 @@ module vmTestBench();
     MAX: coverpoint VM.credit {bins maxC = {255};}
     */
   endgroup
-  
-  // This syntax should work.
+
   covergroup Cross_Cov @ (clk);
     PRODUCTS: coverpoint VM.product {
       bins appleDetected = {0};           //00
@@ -64,34 +66,39 @@ module vmTestBench();
     }
     CROSS_COV: cross PRODUCTS, ERROR;
   endgroup
+/* ~~~~~ */
 
+/* TESTBENCH */
   initial begin
+    // Initialize your rand class and your covergroups
     random_buy rb = new();
     Trans_Cov tc = new();
     Cross_Cov cc = new();
     
-    reg [4:0] arr = 5'b11111;
-    reg index = 0;
-    reg buySig = 0;
-    reg [1:0] prodSig = 2'b00;
+    // Set initial values
+    reg [4:0] arr = 5'b11111;					// Used for coin input.
+    reg index = 0;						// Used to pick which coin input
+    reg buySig = 0;						// Used to pick if buying or not
+    reg [1:0] prodSig = 2'b00;					// Used to pick which product to buy that cycle
     
-    //Set initial values
     clk <= 0;
-    reset <= 0;
+    reset <= 0;							// Guaranteed to start off with a system reset.
+    
     
     //Randomize Inputs
-    repeat (5000) begin
+    repeat (5000) begin						// Maximum amount of test cycles you want to run
       assert (rb.randomize());
       
-      
-      if      (rb.index == 0) begin arr = 5'b01001; end
-      else if (rb.index == 1) begin arr = 5'b00010; end
-      else if (rb.index == 2) begin arr = 5'b01110; end
-      else if (rb.index == 3) begin arr = 5'b01010; end
-      else if (rb.index == 4) begin arr = 5'b11111; end
+      //Chooses which coin input to select
+      if      (rb.index == 0) begin arr = 5'b01001; end		// Penny	
+      else if (rb.index == 1) begin arr = 5'b00010; end		// Nickel
+      else if (rb.index == 2) begin arr = 5'b01110; end		// Dime
+      else if (rb.index == 3) begin arr = 5'b01010; end		// Quarter
+      else if (rb.index == 4) begin arr = 5'b11111; end		// No coin
 
-      if (rb.buyC == 0) begin buySig = 0; end
-      else if (rb.buyC == 1) begin buySig = 0; end
+      //Chooses whether to buy or not
+      if (rb.buyC == 0) begin buySig = 0; end			// Similar to a rand dist, but more fleshed out.
+      else if (rb.buyC == 1) begin buySig = 0; end		// buyC dist {[0:8] := 0, 9 := 1};}
       else if (rb.buyC == 2) begin buySig = 0; end
       else if (rb.buyC == 3) begin buySig = 0; end
       else if (rb.buyC == 4) begin buySig = 0; end
@@ -101,32 +108,22 @@ module vmTestBench();
       else if (rb.buyC == 8) begin buySig = 0; end
       else if (rb.buyC == 9) begin buySig = 1; end
 
-      
+      //Chooses which product to buy
       if (rb.prodSel == 0) begin prodSig = 2'b00; end
       else if (rb.prodSel == 1) begin prodSig = 2'b01; end
       else if (rb.prodSel == 2) begin prodSig = 2'b10; end
       else if (rb.prodSel == 3) begin prodSig = 2'b11; end
       
       // Input code for inputing serialIn from array
-      /*
-      #20     reset <= 1;
-      #40     buy <= buySig;                 // Signal is constant until next run
-      #40     product <= prodSig;            // Signal is constant until next run
-      
-      #40     serialIn <= arr[4];
-      #60     serialIn <= arr[3];
-      #80     serialIn <= arr[2];
-      #100    serialIn <= arr[1];
-      #120    serialIn <= arr[0];
-      */
-      #10 reset = 1;
-      #10 serialIn = arr[4];
-      #10 serialIn = arr[3];
-      #10 serialIn = arr[2];
-      #10 serialIn = arr[1];
-      #10 serialIn = arr[0];
-          buy = buySig;
-          product = prodSig;
+
+      #10 reset = 1;					//@10
+      #10 serialIn = arr[4];				//@20
+      #10 serialIn = arr[3];				//@30
+      #10 serialIn = arr[2];				//@40
+      #10 serialIn = arr[1];				//@50
+      #10 serialIn = arr[0];				//@60		Parallel
+          buy = buySig;					//@60		Parallel
+          product = prodSig;				//@60		Parallel
     end
   end
   always begin
