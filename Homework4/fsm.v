fc3bffd046da99974dabf146a5074a018f4fb8a2
Styxx <Styxx_N_Stones@live.com>
2014-12-16 05:10:42 -0800

    Update fsm.v

--- Homework4/fsm.v
+++ Homework4/fsm.v
@@ -1,17 +1,11 @@
 /*
 *	Finite State Machine
-*	Based off Ford Thunderbird template from 152A
+*	Based off original Ford Thunderbird template from 152A
 *	Vincent "Styxx" Chang
 */
 
 
 module fsm (clk, x, rs, reset, shift, update, run);
-/*
-  `define RS 4'b0001                      //Reset state
-  `define RI 4'b0010                      //Run/Idle state
-  `define SH 4'b0100                      //Shift state
-  `define UP 4'b1000                      //Update state input
-*/
 
   input clk;
   input x;                                //Signal that makes the FSM change state
