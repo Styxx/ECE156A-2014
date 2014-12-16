7f07070647bd4ebccb1158c8a5f07bc6a3394cd6
Styxx <Styxx_N_Stones@live.com>
2014-12-16 00:50:43 -0800

    Update counter.v

--- Homework4/counter.v
+++ Homework4/counter.v
@@ -1,18 +1,18 @@
 /*
 * Behavioural counter up to 10(dec)
 */
-module counter(count, start,a_reset,s_reset,clk);
+module counter(count, start,reset,clk);
   input start, reset, clk;
   output [3:0] count;
 
   reg [3:0] count;
 
-  always @ (posedge clk or reset)
+  always @ (posedge clk or posedge reset)
     if(reset) begin
-	    count = 4'b0;
+	    count <= 4'b0;
     end else if (~start) begin
       if (count == 2'b1010) begin
-        count = 4'b0;
+        count <= 4'b0;
       end else begin
 	      count <= count + 1;
       end
